ee0c98b83ab4dfd6330dc6645c1660f211e8316f
Julian Wegner <Julian.wegner1@gmx.de>
2013-05-12 21:48:00 +0200

    Scraper GUI

---
+++ Scraper GUI.app/Contents/Resources/AppDelegate.rb
@@ -0,0 +1,21 @@
+#
+#  AppDelegate.rb
+#  Scraper GUI
+#
+#  Created by Julian Wegner on 04.05.13.
+#  Copyright 2013 Julian Wegner. All rights reserved.
+#
+
+class AppDelegate
+    attr_accessor :window
+    attr_accessor :textfield
+    
+    def applicationDidFinishLaunching(a_notification)
+        # Insert code here to initialize your application
+    end
+    
+    def Taktart(mouseX)
+        
+    end
+end
+
